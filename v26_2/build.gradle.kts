5ad899b8db5a6deeeac5bc7da5fdf9d447284337
isnsest <gagagaopo@gmail.com>
2026-08-24 20:30:55 +0300

    build(v26_2): update spigot dependency to 26.2-R0.1-SNAPSHOT

--- v26_2/build.gradle.kts
+++ v26_2/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     implementation(project(":core"))
     compileOnly(project(":v1_21"))
-    compileOnly("org.spigotmc:spigot:26.1.2-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot:26.2-R0.1-SNAPSHOT")
     compileOnly("com.denizenscript:denizen:1.3.3-SNAPSHOT")
     compileOnly(fileTree(projectDir.resolve("libs")) { include("*.jar") })
 }
